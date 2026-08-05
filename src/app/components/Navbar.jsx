94b74f18f04ed11b50f82000b07e8528dcc3bc10
Eunice Ogunemi <euniceogunemi@gmail.com>
2026-08-05 14:18:34 +0000

    modification sur les images

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -63,13 +63,8 @@ export default function Navbar() {
 
           {/* Icônes droite */}
           <div className="flex items-center gap-4">
-            {/* Recherche */}
-            <button className="hidden md:flex text-brun-200 hover:text-brun-50 transition-colors">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-4.35-4.35M17 11A6 6 0 1 1 5 11a6 6 0 0 1 12 0z" />
-              </svg>
-            </button>
-
+            
+            
             {/* Panier */}
             <Link
               href="/panier"
